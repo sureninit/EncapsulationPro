78556f964c010173c0c1b053c8a4431ca0dbdde6
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-03-18 15:00:31 -0500

    project upload

---
+++ src/Custumers.java
@@ -0,0 +1,30 @@
+
+public class Custumers {
+	private String fname;
+	private int accountnumber;
+	public String getName() {
+		return fname;
+	}
+
+	public void setName(String name) {
+		this.fname = name;
+	}
+
+	public int getAccountnumber() {
+		return accountnumber;
+	}
+
+	public void setAccountnumber(int accountnumber) {
+		this.accountnumber = accountnumber;
+	}
+
+	
+	
+	public void details() {
+		
+		System.out.println("Details of custumers:"+" "+"name: "+fname+" accountnumber: "+accountnumber);
+		
+	}
+	
+
+}
---
+++ src/Test_Class.java
@@ -0,0 +1,13 @@
+
+public class Test_Class {
+
+	public static void main(String[] args) {
+		Custumers cu = new Custumers();
+		cu.setAccountnumber(30271);
+		cu.setName("jyantaRai");
+		cu.details();
+
+
+	}
+
+}
